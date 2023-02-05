6ff4bd2dda86e9aad54793db171b4de32cd5c45c
Namrah <namrah.ajmal@vanderbilt.edu>
2023-02-05 15:07:14 -0600

    Initial Commit

---
+++ Syrenergy/SyrenergyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyrenergyApp.swift
+//  Syrenergy
+//
+//  Created by NamrahAjmal on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SyrenergyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
