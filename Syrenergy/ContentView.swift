179ffb005afa1b871b7e4cb65e447050aedfe816
Namrah <namrah.ajmal@vanderbilt.edu>
2023-02-05 16:10:08 -0600

    new branch

--- Syrenergy/ContentView.swift
+++ Syrenergy/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            // frontend stuffs
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
